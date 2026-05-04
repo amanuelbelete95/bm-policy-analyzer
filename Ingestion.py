09250b92e77fd8e7cab63fc6439bed805edac334
Amanuel Belete <amanuelbelete95@gmail.com>
2026-05-04 20:42:29 +0300

    add other file type

--- Ingestion.py
+++ Ingestion.py
@@ -1,8 +1,17 @@
 
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import MarkdownLoader
+from langchain_community.document_loaders import TextLoader
 
-def getTextFromDocuments(pdf_path):
-    loader = PyPDFLoader(pdf_path)
+#  What if the file is md or text can this function be used to get the text from the md file? Yes, you can use this function to get the text from the md file as well. You just need to change the loader to a markdown loader. For example, you can use the following code to get the text from a markdown file:
+
+def getTextFromDocuments(file_path):
+    if file_path.endswith(".md"):
+        loader = MarkdownLoader(file_path)
+    elif file_path.endswith(".txt"):
+        loader = TextLoader(file_path)
+    else:
+        loader = PyPDFLoader(file_path)
     documents = loader.load()
     text = ""
     for doc in documents:
